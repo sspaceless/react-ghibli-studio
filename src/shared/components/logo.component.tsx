ca72cf5b1ddab943586d92fcada32c7f600a055e
Illia Bodnar <illia.v.bodnar@gmail.com>
2024-12-16 20:04:41 +0200

    feat: add shadcn components and config

---
+++ src/shared/components/logo.component.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import Image from "next/image";
+
+export const Logo: React.FC = () => {
+  const { resolvedTheme } = useTheme();
+  const darkLogoSrc = `/ghibli-logo-dark.svg`;
+  const lightLogoSrc = `/ghibli-logo-dark.svg`;
+
+  return (
+    <div className="flex flex-row items-center gap-4">
+      {resolvedTheme === "dark" ? (
+        <Image src={darkLogoSrc} alt="Studio Ghibli logo" width={36} height={36} />
+      ) : (
+        <Image src={lightLogoSrc} alt="Studio Ghibli logo" width={36} height={36} />
+      )}
+
+      <div className="flex flex-col items-start justify-center">
+        <span>Studio Ghibli</span>
+        <span>スタジオジブリ</span>
+      </div>
+    </div>
+  );
+};
